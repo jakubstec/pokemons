2355accf595e82c6c37597eb35678bb9ce59648f
jakubstec <kub9stec@gmail.com>
2026-02-23 15:12:24 +0100

    (feat): face recognition

--- app/(tabs)/(favourite)/camera.tsx
+++ app/(tabs)/(favourite)/camera.tsx
@@ -1,46 +1,84 @@
-import { useRef } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useRef } from 'react';
 import {
-  Camera as VisionCamera,
-  Frame,
+  Camera,
+  runAsync,
   useCameraDevice,
   useCameraPermission,
+  useFrameProcessor,
 } from 'react-native-vision-camera';
-
 import {
   Face,
-  Camera,
+  useFaceDetector,
   FrameFaceDetectionOptions,
 } from 'react-native-vision-camera-face-detector';
+import { Worklets } from 'react-native-worklets-core';
 
-export default function CameraScreen() {
+export default function App() {
   const device = useCameraDevice('front');
-  const { hasPermission, requestPermission } = useCameraPermission();
-  const cameraRef = useRef<VisionCamera>(null);
+  const faceDetectionOptions = useRef<FrameFaceDetectionOptions>({
+    performanceMode: 'fast',
+    classificationMode: 'all',
+  }).current;
 
-  const faceDetectionOptions = useRef<FrameFaceDetectionOptions>({}).current;
+  const { detectFaces, stopListeners } = useFaceDetector(faceDetectionOptions);
+  const { hasPermission } = useCameraPermission();
 
-  function handleFacesDetection(faces: Face[], frame: Frame) {
-    console.log('faces', faces.length, 'frame', frame.toString());
-  }
+  useEffect(() => {
+    (async () => {
+      await Camera.requestCameraPermission();
+    })();
+    return () => stopListeners();
+  }, []);
 
-  if (!hasPermission) {
-    requestPermission();
-  }
+  const handleDetectedFaces = Worklets.createRunOnJS((faces: Face[]) => {
+    if (faces.length > 0) {
+      console.log('Detected faces:', faces.length);
+    }
+  });
+
+  const frameProcessor = useFrameProcessor(
+    (frame) => {
+      'worklet';
+      runAsync(frame, () => {
+        'worklet';
+        const faces = detectFaces(frame);
+        handleDetectedFaces(faces);
+      });
+    },
+    [detectFaces, handleDetectedFaces]
+  );
+
   if (!device)
     return (
-      <View>
-        <Text>no device or no permission</Text>
+      <View style={styles.center}>
+        <Text>No Device!</Text>
       </View>
     );
+
+  if (!hasPermission)
+    return (
+      <View style={styles.center}>
+        <Text>No Permission!</Text>
+      </View>
+    );
+
   return (
-    <Camera
-      style={StyleSheet.absoluteFill}
-      device={device}
-      faceDetectionCallback={handleFacesDetection}
-      faceDetectionOptions={faceDetectionOptions}
-      ref={cameraRef}
-      isActive={true}
-    />
+    <View style={{ flex: 1 }}>
+      <Camera
+        style={StyleSheet.absoluteFill}
+        device={device}
+        isActive={true}
+        frameProcessor={frameProcessor}
+      />
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  center: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
